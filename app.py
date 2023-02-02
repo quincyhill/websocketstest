eb450f5b1f156a65c6d9fff996c2815f8d96a08d
quincyhill <therealquincyhill@gmail.com>
2023-02-02 11:14:26 -0600

    Wrote my implementation of the problem and it works

--- app.py
+++ app.py
@@ -4,22 +4,44 @@ import websockets
 from connect4 import PLAYER1, PLAYER2, Connect4
 
 async def handler(websocket):
+    # Game is master state
     game = Connect4()
-    row_count = 0
+
     async for message in websocket:
-        # The only data coming from the client is {'type': 'play', column : int}
+        # Valid in types
+        # {type: 'play', column : INT}
+
         data = json.loads(message)
 
-        # Each click will just add to the first column for the red player, just for now
-        res =  {'type': 'play', 'player': PLAYER1, 'column': 0, 'row': row_count}
-        
-        await websocket.send(json.dumps(res))
+        # Ensure the input data is valid
+        assert data['type'] == 'play'
+        assert type(data['column']) == int
 
-        row_count += 1
+        # Valid out types
+        # {type: "play", player: PLAYER, column: INT, row: INT}
+        # {type: "win", player: PLAYER}
+        # {type: "error", "message": MESSAGE}
+
+        current_player = PLAYER2 if len(game.moves) % 2 else PLAYER1
+
+        try:
+            row = game.play(current_player, data['column'])
+            res = {'type': 'play', 'player': current_player, 'column': data['column'], 'row': row}
+
+            await websocket.send(json.dumps(res))
+
+            if game.last_player_won:
+                res = {'type': 'win', 'player': game.last_player}
+                await websocket.send(json.dumps(res))
+
+        except Exception as e:
+            res = {'type': 'error', 'message': str(e)}
+            await websocket.send(json.dumps(res))
 
 async def main():
-    async with websockets.serve(ws_handler=handler, host="", port=8001):
-        print("Server started")
+    PORT = 8001
+    async with websockets.serve(ws_handler=handler, host="", port=PORT):
+        print(f"Server started on port {PORT}")
         await asyncio.Future()
 
 if __name__ == "__main__":
